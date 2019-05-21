d1b964511e35635a23ab9ff3b7b82547950fe159
Graham Gilpatric <grahamwithag@gmail.com>
2019-05-21 00:19:48 -0400

    reads HTML file and serves it

--- server.js
+++ server.js
@@ -1,7 +1,17 @@
 const http = require('http');
+const fileSystem = require('fs');
+const path = require('path');
 
 const server = http.createServer((request, response) => {
-  response.end('huh');
+  const filePath = path.join(__dirname, '/doc.html')
+
+  fileSystem.readFile(filePath, 'utf8', (error, data) => {
+    if (error) {
+      throw error;
+    }
+    response.writeHeader(200, {'Content-Type': 'text/html'})
+    response.end(data);
+  })
 });
 
 server.listen(3009);
